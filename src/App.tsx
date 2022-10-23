9ff2951ce0e51405ba75f8625875d488f1987f12
kaikySantos <info.kaiky@gmail.com>
2022-10-23 20:33:37 -0300

    feat: global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 import { Home } from './pages/Home'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <GlobalStyle />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
