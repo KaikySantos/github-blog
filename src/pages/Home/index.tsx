2c2a522ea7452c5984271db469275b5b8b7a6457
kaikySantos <info.kaiky@gmail.com>
2022-10-24 12:52:31 -0300

    feat: cards

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { InfoProfile } from './InfoProfile'
-import { HomeContainer, InputSearch } from './styles'
+import { Card, CartsContainer, HomeContainer, InputSearch } from './styles'
 
 export function Home() {
   return (
@@ -13,6 +13,107 @@ export function Home() {
         </div>
 
         <InputSearch placeholder="Buscar conteúdo" />
+
+        <CartsContainer>
+          <Card>
+            <header>
+              <h4>JavaScript data types and data structures</h4>
+              <span>Há 1 dia</span>
+            </header>
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn.
+            </p>
+          </Card>
+          <Card>
+            <header>
+              <h4>JavaScript data types and data structures</h4>
+              <span>Há 1 dia</span>
+            </header>
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn.
+            </p>
+          </Card>
+          <Card>
+            <header>
+              <h4>JavaScript data types and data structures</h4>
+              <span>Há 1 dia</span>
+            </header>
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn.
+            </p>
+          </Card>
+          <Card>
+            <header>
+              <h4>JavaScript data types and data structures</h4>
+              <span>Há 1 dia</span>
+            </header>
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn.
+            </p>
+          </Card>
+          <Card>
+            <header>
+              <h4>JavaScript data types and data structures</h4>
+              <span>Há 1 dia</span>
+            </header>
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn.
+            </p>
+          </Card>
+          <Card>
+            <header>
+              <h4>JavaScript data types and data structures</h4>
+              <span>Há 1 dia</span>
+            </header>
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn.
+            </p>
+          </Card>
+          <Card>
+            <header>
+              <h4>JavaScript data types and data structures</h4>
+              <span>Há 1 dia</span>
+            </header>
+            <p>
+              Programming languages all have built-in data structures, but these
+              often differ from one language to another. This article attempts
+              to list the built-in data structures available in JavaScript and
+              what properties they have. These can be used to build other data
+              structures. Wherever possible, comparisons with other languages
+              are drawn.
+            </p>
+          </Card>
+        </CartsContainer>
       </section>
     </HomeContainer>
   )
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -6,11 +6,12 @@ export const HomeContainer = styled.div`
   max-width: 54rem;
   margin: 0 auto;
   padding: 0 1.5rem;
+  padding-bottom: 2rem;
 
   section {
     margin-top: 4.5rem;
 
-    div {
+    div:first-child {
       display: flex;
       justify-content: space-between;
 
@@ -39,5 +40,57 @@ export const InputSearch = styled.input`
 
   &::placeholder {
     color: ${(props) => props.theme['base-label']};
+  }
+`
+
+export const CartsContainer = styled.div`
+  margin-top: 3rem;
+
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 2rem;
+
+  @media (max-width: 700px) {
+    grid-template-columns: 1fr;
+  }
+`
+
+export const Card = styled.div`
+  border: 2px solid transparent;
+  background: ${(props) => props.theme['base-post']};
+  border-radius: 10px;
+  padding: 2rem;
+  cursor: pointer;
+
+  display: flex;
+  flex-direction: column;
+
+  header {
+    display: flex;
+    font-size: 1.25rem;
+    color: ${(props) => props.theme['base-title']};
+    margin-bottom: 1.25rem;
+
+    span {
+      white-space: nowrap;
+      font-size: 0.875rem;
+      color: ${(props) => props.theme['base-span']};
+      margin-left: 1rem;
+    }
+  }
+
+  p {
+    color: ${(props) => props.theme['base-text']};
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-lines: 3;
+    display: -webkit-box;
+    -webkit-line-clamp: 3;
+    -webkit-box-orient: vertical;
+  }
+
+  &:hover {
+    border-color: ${(props) => props.theme['base-label']};
+    transition: border-color 0.3s;
   }
 `
